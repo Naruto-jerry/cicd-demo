1dbe330252c45c6e60e10ccbb4b0e651bd057106
Jagruti Indurkar <jagruti.indurkar@outlook.com>
2025-03-24 01:50:51 +0530

    Add greeting endpoint

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello, This is jagrutis CI/CD World!');
+    res.send('Hello, This is Jagruti CI/CD World!');
+});
+
+app.get('/greeting', (req, res) => {
+    res.send('Welcome ! just learning CI/CD pipelines, so lets gooo');
 });
 
 app.listen(port, () => {
